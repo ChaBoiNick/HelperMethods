d6fdd964995c6879e2b03584f2b9b2241cb43d3c
Nicholas Schmidt <talkschmidt@hotmail.com>
2018-08-02 12:26:15 +1000

    Add project files.

---
+++ HelperMethods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Name Game");
+
+            Console.Write("What's your first name? ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What's your last name? ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("In what city were you born? ");
+            string city = Console.ReadLine();
+
+            Console.Write("Results: ");
+            ReverseString(firstName);
+            ReverseString(lastName);
+            ReverseString(city);
+
+            Console.ReadLine();
+
+        }
+
+        private static void ReverseString(string message)
+        {            
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            foreach (char item in messageArray)
+            {
+                Console.Write(item);
+            }
+            Console.Write(" "); // adds spaces between strings
+        }
+
+
+    }
+}
